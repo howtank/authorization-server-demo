6e856e164cb785dc94c09b4bb86f09fbf8d4f7c2
jeanpaulb <jean-paul@howtank.com>
2022-02-17 11:59:09 +0100

    updated redirect URI for debug

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ const configuration = {
 		token_endpoint_auth_method: 'none',
 		response_types: ['id_token'],
 		grant_types: ['implicit'],
-		token_endpoint_auth_method: 'none',
 
-		// hardcoded for the example only
     client_id: 'id_provided_by_howtank',
-    redirect_uris: ['https://community.papote.co/'],
+    redirect_uris: ['https://jwt.io'],
   }],
 };
 
 const oidc = new Provider('http://localhost:3000', configuration);
+
+// uncomment the following block to test locally
+/* const { invalidate: orig } = oidc.Client.Schema.prototype;
+
+oidc.Client.Schema.prototype.invalidate = function invalidate(message, code) {
+  if (code === 'implicit-force-https' || code === 'implicit-forbid-localhost') {
+    return;
+  }
+
+  orig.call(this, message);
+}; */
+// end uncomment to test locally
 
 oidc.listen(3000, () => {
   console.log('oidc-provider listening on port 3000, check http://localhost:3000/.well-known/openid-configuration');
